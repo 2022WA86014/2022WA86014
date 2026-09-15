4c0ac9fa1631f309ad36af4bd7853e1df24c3bdf
Rohit R Deore <2022wa86014@wilp.bits-pilani.ac.in>
2026-09-15 22:05:16 +0530

    Update application message to validate CI

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -2,6 +2,6 @@ package com.bits.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps CI build successful - 2022WA86014");
+        System.out.println("Contineous Integration triggered successfully - 2022WA86014");
     }
 }
